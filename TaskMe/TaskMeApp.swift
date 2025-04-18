aea2965339df9cc02360a8db7845621700ba71ad
Benjamin <dotsehyde@gmail.com>
2025-04-18 10:34:33 +0000

    Initial Commit

---
+++ TaskMe/TaskMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMeApp.swift
+//  TaskMe
+//
+//  Created by Benjamin. on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
